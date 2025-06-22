7288188ce74a9eabe706da02487717b390a9a9ba
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-06-22 14:54:50 +0300

    feat: add Track::incrementNumberOfViewsForTrackWithId() method

--- app/Models/Track.php
+++ app/Models/Track.php
@@ -11,4 +11,8 @@ class Track extends Model {
   public static function find($id) {
     return static::get("track/$id");
   }
+  public static function incrementNumberOfViewsForTrackWithId($id) {
+    // the endpoint returns a track with numberOfViews field incremented successfully
+    return static::patch("track/$id/numberOfViews");
+  }
 }
