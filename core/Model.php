c80a3f4ad6fa43b7409a66dc62ff2313e85a30ad
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-06-22 14:53:04 +0300

    feat: add patch method to core Model class

--- core/Model.php
+++ core/Model.php
@@ -16,4 +16,24 @@ class Model {
     }
     return json_decode($res, true);
   }
+
+  protected static function patch($endpoint) {
+    $url = static::$basePath . $endpoint;
+
+    $context_options = [
+      'http' => [
+        'method' => 'PATCH',
+        'header' => 'Content-Type: application/json',
+        'content' => '',
+      ]
+      ];
+    $context = stream_context_create($context_options);
+
+    $res = @file_get_contents($url, false, $context);
+    // the var_dumps aren't displayed in the response from the api on postman 
+    /* Wrong, they finally got displayed */
+    // var_dump('this is patch res!');
+    // var_dump(json_decode($res)); // null
+    return json_decode($res, true);
+  }
 }
