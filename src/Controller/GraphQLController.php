83ac0c6ad51f05f7afbaa076b3b45cf36ec95ea7
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-06-22 14:58:02 +0300

    feat: add incrementTrackNumberOfViews mutation (our first mutation in the course!)

--- src/Controller/GraphQLController.php
+++ src/Controller/GraphQLController.php
@@ -5,6 +5,7 @@ namespace Src\Controller;
 use App\Models\Track;
 use App\Models\Author;
 use App\Models\Module;
+use ErrorException;
 use GraphQL\Error\DebugFlag;
 use GraphQL\GraphQL;
 use GraphQL\Type\Definition\ObjectType;
@@ -69,6 +70,28 @@ class TrackType extends ObjectType {
   }
 }
 
+class IncrementTrackNumberOfViewsResponse extends ObjectType {
+  public function __construct() {
+    $config = [
+      'fields' => [
+        'code' => Type::int(),
+        'success' => Type::boolean(),
+        'message' => Type::string(),
+        // 'track' => new Track(), // Wrong! will throw an error because webonyx-gql expects field config to be an array
+        'track' => [
+          /* Another error! Object of type App\Models\Track is not callable. 
+          Reason of the error: The type you provide to the type key should be a class extending ObjectType
+          provided by the library, not the Track model we created! 
+          */
+          // 'type' => new Track(),
+          'type' => new TrackType(),
+        ]
+      ]
+    ];
+    parent::__construct($config);
+  }
+}
+
 class GraphQLController {
   public static function handle() {
     $queryType = new ObjectType([
@@ -93,8 +116,69 @@ class GraphQLController {
       ]
     ]);
 
+    $mutationType = new ObjectType([
+      'name' => 'Mutation',
+      'fields' => [
+        'incrementTrackNumberOfViews' => [
+          'type' => new IncrementTrackNumberOfViewsResponse(),
+          'args' => [
+            'id' => Type::id(),
+          ],
+          /* The solution by checking against the track if it is null or not */
+          // 'resolve' => function($_, $args) {
+          //     $track = Track::incrementNumberOfViewsForTrackWithId($args['id']);
+          //     // var_dump($track);//null
+          //     if(is_null($track)) {
+          //       return [
+          //       'code' => 500,
+          //       'success' => false,
+          //       'message' => 'An error occurred. Please try again later',
+          //       'track' => $track
+          //     ];
+          //     }
+
+          //     return [
+          //       'code' => 200,
+          //       'success' => true,
+          //       'message' => "Hooray! The numberOfViews for track of id: {$args['id']} were updated successfully",
+          //       'track' => null
+          //     ];
+            
+          //   }
+
+          /* Modifying the default error handler and restoring it after the
+          resolver function exection*/
+          'resolve' => function($_, $args) {
+            $prev_handler = set_error_handler(function($errno, $errstring, $errfile, $errline) {
+              throw new ErrorException($errstring, 0, $errno, $errfile, $errline);
+            });
+            try {
+              $track = Track::incrementNumberOfViewsForTrackWithId($args['id']);
+              // var_dump($track);//null
+              return [
+                'code' => 200,
+                'success' => true,
+                'message' => "Hooray! The numberOfViews for track of id: {$args['id']} were updated successfully",
+                'track' => $track
+              ];
+            } catch(\Throwable $th) {
+              return [
+                'code' => 500,
+                'success' => false,
+                'message' => 'An error occurred. Please try again later',
+                'track' => null
+              ];
+            } finally {
+              restore_error_handler();
+            }
+          }
+        ]
+      ]
+    ]);
+
     $schema = new Schema([
-      'query' => $queryType
+      'query' => $queryType,
+      'mutation' => $mutationType
     ]);
 
     $requestBody = file_get_contents('php://input'); // Raw JSON string from HTTP request
